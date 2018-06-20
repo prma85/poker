b955120fd4b739a347b97cd7e785caac77a3190b
Paulo Andrade <paulo85br@gmail.com>
2018-06-20 13:09:15 -0600

    index finished
    all index finished, working on poker library

--- index.js
+++ index.js
@@ -4,4 +4,70 @@
  * and open the template in the editor.
  */
 
+function createRoom() {
+    //get the number of players in the game to create the room and setting that it is a decimal number
+    var nplayers = parseInt($("#nplayers").val(), 10);
 
+    //doing the validation for number of players
+    if (nplayers < 2) {
+        alert("You need at least 2 players to play this game");
+        return false;
+    } else if (nplayers > 10) {
+        alert("Let focus in a game with 10 players or less ;)");
+        return false;
+    }
+
+    //creating the room (players and cards inputs)
+    var html = generateRoomHTML(nplayers);
+    //console.log(html);
+    
+    //adding the html and inputs in the form
+    $("#game").html(html);
+    $("#game").submit(checkWinner);
+    
+    
+    //show the hidden form
+    $("#newroom").show();
+    
+    //show the game rules to play
+    $("#gameRules").show();
+}
+
+function generateRoomHTML(n){
+   var html = '';
+
+    //creating the inputs for each player
+   for (var i = 1; i<=n; i++){
+     html += '<fieldset>';
+     html += '<legend>Player' + i + '</legend>';
+     html += '<div class="row">';
+     
+     //player name
+     html += '<div class="column">';
+     html += '<input required type="text" name="player_'+i+'" id="player_'+i+'" placeholder="Inform the player name" />';
+     html += '</div>';
+     
+     //cards input
+     html += '<div class="column">';
+     html += '<input required type="text" name="player_cards_'+i+'" id="player_cards_'+i+'" placeholder="Inform the player\'s cards" />';
+     html += '</div>';
+     
+     //closes row
+     html += '</div>';
+     
+     //closes fieldset
+     html += '</fieldset>';
+   }
+   
+    html += '<input class="button-primary" type="submit" value="Check winner">';
+
+    return html;
+}
+
+function checkWinner(event){
+    event.preventDefault();
+    var poker = new Poker();
+    poker.checkWinner($('#'+event.target.id).serializeArray());
+    //show how to score in the game
+    $("#gamePoints").show();
+}
--- poker.js
+++ poker.js
@@ -4,4 +4,10 @@
  * and open the template in the editor.
  */
 
-
+class Poker {
+    checkWinner (game){
+        var size = game.length;
+        console.log(size);
+        alert(game);
+    }
+}
